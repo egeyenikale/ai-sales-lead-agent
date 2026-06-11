76035c1191173419aa2127ccf52aac50ee77c0df
Ege Hidayet Koca <kocaege777@gmail.com>
2026-06-11 12:21:00 +0300

    feat: configure persistence or state layer

--- src/implementation-roadmap.js
+++ src/implementation-roadmap.js
@@ -8,3 +8,8 @@ export const stage2 = {
   "date": "2026-06-09",
   "message": "feat: add core domain model"
 };
+
+export const stage3 = {
+  "date": "2026-06-11",
+  "message": "feat: configure persistence or state layer"
+};
